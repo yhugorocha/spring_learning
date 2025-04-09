608853bc8e1efcad7e85833f79ecc40fe9810c9b
Hugo Silva <jose.silva@extreme.digital>
2025-04-09 17:55:50 -0300

    feat: Implementing solicitation controller

---
+++ src/main/java/io/git/yhugorocha/learning/controller/SolicitationController.java
@@ -0,0 +1,32 @@
+package io.git.yhugorocha.learning.controller;
+
+import io.git.yhugorocha.learning.dto.Solicitation;
+import io.git.yhugorocha.learning.service.SolicitationService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/v1/solicitacoes")
+public class SolicitationController {
+
+    private final SolicitationService solicitationService;
+
+    @PostMapping
+    public ResponseEntity<Solicitation> createSolicitation(@RequestBody Solicitation solicitation){
+        return ResponseEntity.ok(solicitationService.create(solicitation));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Solicitation>> getSolicitations(){
+        return ResponseEntity.ok(solicitationService.findAll());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Solicitation> updateSolicitation(@PathVariable("id") Long id, @RequestBody Solicitation solicitation){
+        return ResponseEntity.ok(solicitationService.update(id, solicitation));
+    }
+}
