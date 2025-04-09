0a0254778d6cfd7121400ec67baa62503f9f4a7e
Hugo Silva <jose.silva@extreme.digital>
2025-04-09 09:07:09 -0300

    first commit

---
+++ src/test/java/io/git/yhugorocha/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package io.git.yhugorocha.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
