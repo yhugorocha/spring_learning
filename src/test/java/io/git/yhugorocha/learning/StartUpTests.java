fa81ef2456b2bbc7b1aef537b15adf5c8a070036
Hugo Silva <yhugorocha@gmail.com>
2025-06-11 22:46:49 -0300

    feat: Adding Test Class and configuring Mapstruct and Lombok to work together

--- src/test/java/io/git/yhugorocha/learning/StartUpTests.java
+++ src/test/java/io/git/yhugorocha/learning/StartUpTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class LearningApplicationTests {
+class StartUpTests {
 
 	@Test
 	void contextLoads() {
---
+++ src/test/java/io/git/yhugorocha/learning/service/SolicitationServiceTest.java
@@ -0,0 +1,24 @@
+package io.git.yhugorocha.learning.service;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SolicitationServiceTest {
+
+
+
+    @Test
+    @DisplayName("Test create Solicitation")
+    void create() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
