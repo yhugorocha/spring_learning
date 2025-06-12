53d71f424eb06c0fcc75dc5704040e8a194cf5dd
Hugo Silva <yhugorocha@gmail.com>
2025-06-11 23:06:12 -0300

    feat: Creating the first test method

--- src/test/java/io/git/yhugorocha/learning/service/SolicitationServiceTest.java
+++ src/test/java/io/git/yhugorocha/learning/service/SolicitationServiceTest.java
@@ -1,24 +1,52 @@
 package io.git.yhugorocha.learning.service;
 
+import io.git.yhugorocha.learning.dto.Solicitation;
+import io.git.yhugorocha.learning.entities.SolicitationEntity;
+import io.git.yhugorocha.learning.mapper.SolicitationMapper;
+import io.git.yhugorocha.learning.repository.SolicitationRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.verify;
 
+@ExtendWith(MockitoExtension.class)
 class SolicitationServiceTest {
 
+    @Mock
+    private SolicitationRepository solicitationRepository;
+    @Mock
+    private SolicitationMapper solicitationMapper;
+    private SolicitationService solicitationService;
 
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        solicitationService = spy(new SolicitationService(solicitationRepository, solicitationMapper));
+    }
 
     @Test
     @DisplayName("Test create Solicitation")
-    void create() {
-    }
+    void testCreate() {
+        Solicitation solicitation = mock(Solicitation.class);
+        SolicitationEntity entity = mock(SolicitationEntity.class);
+        Solicitation resultDto = mock(Solicitation.class);
 
-    @Test
-    void update() {
-    }
+        doReturn(entity).when(solicitationMapper).toSolicitationEntity(solicitation);
+        doReturn(resultDto).when(solicitationMapper).toSolicitation(entity);
+        doReturn(entity).when(solicitationRepository).save(any(SolicitationEntity.class));
 
-    @Test
-    void findAll() {
+        Solicitation result = solicitationService.create(solicitation);
+
+        verify(solicitationMapper).toSolicitationEntity(any(Solicitation.class));
+        verify(solicitationMapper).toSolicitation(any(SolicitationEntity.class));
+        verify(solicitationRepository).save(any(SolicitationEntity.class));
+        assertEquals(resultDto, result);
     }
 }
